a8311a5ece65ed0f8b5c6b4235041914b7aefb8f
neuromancerzei <36718898+neuromancerzei@users.noreply.github.com>
2023-10-18 14:59:38 -0400

    Update cs511hw06.lean

--- Math2001/Homework/cs511hw06.lean
+++ Math2001/Homework/cs511hw06.lean
@@ -14,54 +14,76 @@ def Tribalanced (x : ℝ) : Prop := ∀ n : ℕ, (1 + x / n) ^ n < 3
 
 def Superpowered (k : ℕ) : Prop := ∀ n : ℕ, Prime (k ^ k ^ n + 1)
 
-/- Section 5.1.7 Exercise 11-14 -/
+/- MoP Section 5.1.7 Exercise 11-14 -/
 example {P Q : α → Prop} (h : ∀ x, P x ↔ Q x) : (∃ x, P x) ↔ (∃ x, Q x) := by
   constructor
   · rintro ⟨x, hx⟩
     use x
-    obtain ⟨h, _ ⟩ := h x
-    exact h hx
+    exact (h x).mp hx
   · rintro ⟨x, hx⟩
     use x
-    obtain ⟨_, h ⟩ := h x
-    exact h hx
+    exact (h x).mpr hx
 
 example (P : α → β → Prop) : (∃ x y, P x y) ↔ ∃ y x, P x y := by
   constructor
-  repeat{rintro ⟨x, ⟨y, _⟩⟩; use y, x; assumption}
+  · rintro ⟨x, y, hp⟩
+    exact ⟨y, x, hp⟩
+  · rintro ⟨y, x, hp⟩
+    exact ⟨x, y, hp⟩
 
 example (P : α → β → Prop) : (∀ x y, P x y) ↔ ∀ y x, P x y := by
   constructor
-  repeat{exact fun (h y x) => h x y}
+  · intros h y x
+    exact h x y
+  · intros h x y
+    exact h y x
 
 example (P : α → Prop) (Q : Prop) : ((∃ x, P x) ∧ Q) ↔ ∃ x, (P x ∧ Q) := by
   constructor
-  · rintro ⟨⟨x, _⟩, _⟩
-    use x
-    exact ⟨by assumption, by assumption⟩
-  · rintro ⟨x, ⟨_, _⟩⟩
-    exact ⟨by use x; assumption, by assumption⟩
+  · rintro ⟨⟨x, hp⟩, hq⟩
+    exact ⟨x, hp, hq⟩
+  · rintro ⟨x, hp, hq⟩
+    exact ⟨⟨x, hp⟩, hq⟩
 
-/- Section 5.2.7 Exercise 1-/
-
+/- MoP Section 5.2.7 Exercise 1-/
 example : ∃ x : ℝ, Tribalanced x ∧ ¬ Tribalanced (x + 1) := by
-  have h_lemma1 : Tribalanced 0 := by 
-    intro n
-    ring_nf
-    numbers
-  have h_lemma2 : ¬Tribalanced 2 := by
-    intro h
-    specialize h 2
-    numbers at h
-  by_cases Tribalanced 1
-  · use 1
-    ring_nf
-    constructor <;> assumption
-  · use 0
-    ring_nf
-    constructor <;> assumption
+  by_cases h0: Tribalanced 1
+  . use 1
+    dsimp[Tribalanced] at *
+    constructor
+    . apply h0
+    . simp
+      use 1
+      numbers
+  . use 0
+    constructor
+    . intro n
+      conv => ring_nf
+      numbers
+    . conv => ring_nf
+      apply h0
 
-/- Section 5.2.7 Exercise 3-/
+/- MoP Section 5.2.7 Exercise 3 -/
+theorem superpowered_one : Superpowered 1 := by
+  intro n
+  conv => ring_nf
+  exact prime_two
+
+theorem not_superpowered_three : ¬ Superpowered 3 := by
+  intro h
+  dsimp [Superpowered] at h
+  have four_prime : Prime (3 ^ 3 ^ 0 + 1) := h 0
+  conv at four_prime => numbers -- simplifies that statement to `Prime 4`
+  have four_not_prime : ¬ Prime 4
+  · apply not_prime 2 2
+    · numbers -- show `2 ≠ 1` 
+    · numbers -- show `2 ≠ 4` 
+    · numbers -- show `4 = 2 * 2`
+  contradiction
 
 example : ∃ k : ℕ, Superpowered k ∧ ¬ Superpowered (k + 1) := by
-  sorry
+  by_cases Superpowered 2
+  · use 2
+    exact ⟨h, not_superpowered_three⟩
+  · use 1 
+    exact ⟨superpowered_one, h⟩
